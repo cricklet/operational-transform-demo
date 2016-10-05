68ad51993c9c473dd119a86a99ff4492e0f170b5
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-10-04 22:47:02 -0700

    Start implementing site priorities based on logs

--- ot-lib/sites.js
+++ ot-lib/sites.js
@@ -1,20 +1,21 @@
 /* @flow */
 
-import { genUid } from './utils.js'
-import type { TextOperation } from './operations.js'
+import { genUid, Greater, Equal, Less } from './utils.js'
+import type { Comparison } from './utils.js'
+import type { TextOperation, DeleteOperation, InsertOperation } from './operations.js'
 
 export type LogEntry = {
   sourceSite: Site,
   sourceState: SiteState,
-  operation: TextOperation,
+  sourceOperation: DeleteOperation | InsertOperation, // untransformed
   priority: Priority
 }
+
+export type Log = Array<LogEntry>
 
 export type SiteState = {
   [site: Site]: number // how many operations from some site have been executed here?
 }
-
-export type Log = Array<Log>
 
 export type Priority = Array<Site>;
 export type Site = number;
@@ -23,6 +24,14 @@ export function generateSite(): Site {
   return Math.round(Math.random() * 1000);
 }
 
-export function generatePriority(operation: TextOperation, site: Site, state: SiteState): Priority {
-  throw "Not implemented"
+export function comparePriorities(p0: Priority, p1: Priority): Comparison {
+  throw "wat"
+}
+
+export function generatePriority(operation: DeleteOperation | InsertOperation, site: Site, log: Log): Priority {
+  let conflictingLog = log.filter((logEntry: LogEntry) => {
+    return logEntry.sourceOperation.position == operation.position
+  })
+
+  throw "wat"
 }
