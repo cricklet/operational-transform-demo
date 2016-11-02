df18b90dc021100eaa145a90f325b37d3b69e8b9
Kenrick Rilee <kenrick@plangrid.com>
2016-11-02 13:54:24 -0700

    Observer pattern for arrays & objects

---
+++ js/ot/observe.js
@@ -0,0 +1,54 @@
+
+type Observer = ((changes: any) => void)
+
+export function unobserveArray <O> (array: Array<O>, observer: Observer): void {
+  Array.unobserve(array, observer)
+}
+
+export function unobserveObject <O> (object: O, observer: Observer): void {
+  Object.unobserve(object, observer)
+}
+
+export function observeArray <O> (
+  objects: Array<O>,
+  onAdd: (obj: O) => void,
+  onRemove: (obj: O) => void
+): Observer {
+  let observer = (changes) => {
+    for (let change of changes) {
+      if (change.type === 'splice') {
+        for (let removed of change.removed) {
+          onRemove(removed)
+        }
+        for (let i = 0; i < change.addedCount; i ++) {
+          onAdd(objects[change.index + i])
+        }
+      }
+    }
+  }
+  Array.observe(objects, observer)
+  return observer
+}
+
+export function observeObject <O> (
+  object: O,
+  onKeyAdded: (obj: O, key: string) => void,
+  onKeyRemoved: (obj: O, key: string) => void,
+  onKeyChanged: (obj: O, key: string) => void
+): Observer {
+  let observer = (changes) => {
+    for (let change of changes) {
+      if (change.type === 'add') {
+        onKeyAdded(object, change.name)
+      }
+      if (change.type === 'update') {
+        onKeyChanged(object, change.name)
+      }
+      if (change.type === 'delete') {
+        onKeyRemoved(object, change.name)
+      }
+    }
+  }
+  Object.observe(object, observer)
+  return observer
+}
---
+++ js/ot/observe.spec.js
@@ -0,0 +1,152 @@
+/* @flow */
+
+"use strict"
+
+import { expect } from 'chai'
+import { spy } from 'sinon'
+
+// @flow-ignore
+import { assert, should } from 'chai'
+
+import {
+  observeArray,
+  unobserveArray,
+  observeObject,
+  unobserveObject,
+} from './observe'
+
+describe('observeObject', () => {
+  it ('onKeyAdded works', (done) => {
+    let obj: {[key: string]: any} = {'a': 1}
+
+    let onKeyAdded = spy()
+    let onKeyRemoved = spy()
+    let onKeyChanged = spy()
+
+    let observer = observeObject(obj, onKeyAdded, onKeyRemoved, onKeyChanged)
+
+    obj['b'] = 2
+
+    setTimeout(() => {
+      debugger;
+      assert.isTrue(onKeyAdded.calledOnce)
+      assert.isTrue(onKeyAdded.calledWith(obj, 'b'))
+      assert.isFalse(onKeyRemoved.calledOnce)
+      assert.isFalse(onKeyChanged.calledOnce)
+      done()
+    })
+  })
+  it ('onKeyRemoved works', (done) => {
+    let obj: {[key: string]: any} = {'a': 1}
+
+    let onKeyAdded = spy()
+    let onKeyRemoved = spy()
+    let onKeyChanged = spy()
+
+    let observer = observeObject(obj, onKeyAdded, onKeyRemoved, onKeyChanged)
+
+    delete obj['a']
+
+    setTimeout(() => {
+      debugger;
+      assert.isFalse(onKeyAdded.calledOnce)
+      assert.isTrue(onKeyRemoved.calledOnce)
+      assert.isTrue(onKeyRemoved.calledWith(obj, 'a'))
+      assert.isFalse(onKeyChanged.calledOnce)
+      done()
+    })
+  })
+  it ('onKeyChanged works', (done) => {
+    let obj: {[key: string]: any} = {'a': 1}
+
+    let onKeyAdded = spy()
+    let onKeyRemoved = spy()
+    let onKeyChanged = spy()
+
+    let observer = observeObject(obj, onKeyAdded, onKeyRemoved, onKeyChanged)
+
+    obj['a'] = 3
+
+    setTimeout(() => {
+      debugger;
+      assert.isFalse(onKeyAdded.calledOnce)
+      assert.isFalse(onKeyRemoved.calledOnce)
+      assert.isTrue(onKeyChanged.calledOnce)
+      assert.isTrue(onKeyChanged.calledWith(obj, 'a'))
+      done()
+    })
+  })
+  it ('onKeyChanged works', (done) => {
+    let obj: {[key: string]: any} = {'a': 1}
+
+    let onKeyAdded = spy()
+    let onKeyRemoved = spy()
+    let onKeyChanged = spy()
+
+    let observer = observeObject(obj, onKeyAdded, onKeyRemoved, onKeyChanged)
+
+    obj['a'] = 3
+
+    setTimeout(() => {
+      debugger;
+      assert.isFalse(onKeyAdded.calledOnce)
+      assert.isFalse(onKeyRemoved.calledOnce)
+      assert.isTrue(onKeyChanged.calledOnce)
+      assert.isTrue(onKeyChanged.calledWith(obj, 'a'))
+      done()
+    })
+  })
+})
+
+describe('observeArray', () => {
+  it ('onAdd works', (done) => {
+    let arr = [1,2,3]
+    let onAdd = spy()
+    let onRemove = spy()
+
+    let observer = observeArray(arr, onAdd, onRemove)
+
+    arr.push(4)
+
+    setTimeout(() => {
+      assert.isTrue(onAdd.calledOnce)
+      assert.isTrue(onAdd.calledWith(4))
+      assert.isFalse(onRemove.calledOnce)
+      done()
+    })
+  })
+  it ('onRemove works', (done) => {
+    let arr = [1,2,3]
+    let onAdd = spy()
+    let onRemove = spy()
+
+    let observer = observeArray(arr, onAdd, onRemove)
+
+    arr.pop()
+
+    setTimeout(() => {
+      assert.isFalse(onAdd.calledOnce)
+      assert.isTrue(onRemove.calledOnce)
+      assert.isTrue(onRemove.calledWith(3))
+      done()
+    })
+  })
+  it ('unobserveArray works', (done) => {
+    let arr = [1,2,3]
+    let onAdd = spy()
+    let onRemove = spy()
+
+    let observer = observeArray(arr, onAdd, onRemove)
+
+    unobserveArray(arr, observer)
+
+    arr.push(4)
+    arr.pop()
+
+    setTimeout(() => {
+      assert.isFalse(onAdd.calledOnce)
+      assert.isFalse(onRemove.calledOnce)
+      done()
+    })
+  })
+})
