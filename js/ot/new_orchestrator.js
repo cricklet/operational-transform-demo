9e27cd7d074f9ec294d80a4088d972996165096a
Kenrick Rilee <kenrick.rilee@gmail.com>
2017-01-31 13:00:42 -0800

    Eek, finished working at Coderpad

--- js/ot/new_orchestrator.js
+++ js/ot/new_orchestrator.js
@@ -6,48 +6,40 @@ import { concat, flatten, popRandom, maybePush, hash, clone, merge, last, genUid
 import { find, map, reject, filter } from 'wu'
 
 type ContextualOperation<O> = {
-  childHash: StateHash,
-  parentHash: StateHash,
+  nextIndex: number,
+  index: number,
   subops: O[],
   operationId: OperationId // id which stays the same throughout transforms
 }
 
 type SiteUid = string
-type StateHash = string
 type OperationId = string
 
 // <server-url>/operations POST
 //    client sends operation A, parented on S
 //    server responds with all operations after S and A'
 
-export type ClientEditRequest<O> = {
-  kind: 'ClientEditRequest',
+export type ClientEdit<O> = {
+  kind: 'ClientEdit',
   operation: ContextualOperation<O>, // operation parented in server space
 }
 
-export type ServerEditResponse<O> = {
-  kind: 'ServerEditResponse',
+export type ServerEditResopnse<O> = {
+  kind: 'ServerEditResopnse',
   intermediateOperation: ContextualOperation<O>,
   transformedOperation: ContextualOperation<O>,
 }
 
-// server simply broadcasts operations as it receives them
+export type ClientAck<O> = {
+  kind: 'ClientAck',
+  lastIndex: number // the last server op we've handled
+}
 
-export type ServerBroadcast<O> = {
-  kind: 'ServerBroadcast',
+export type ServerUpdate<O> = {
+  kind: 'ServerUpdate',
   operation: ContextualOperation<O>,
 }
 
-// client can request a complete state reset
-
-export type ClientResetRequest = {
-  kind: 'ClientResetRequest'
-}
-
-export type ServerResetResponse<S> = {
-  kind: 'ServerResetResponse',
-  state: S
-}
 
 //
 
@@ -60,12 +52,12 @@ export class ContextualOperator<O, S> {
     this.applier = applier
   }
 
-  empty (hash: StateHash): ContextualOperation<O> {
+  empty (index: number): ContextualOperation<O> {
     return {
       subops: [],
       operationId: genUid(),
-      parentHash: hash,
-      childHash: hash
+      index: index,
+      nextIndex: index
     }
   }
 
@@ -74,81 +66,48 @@ export class ContextualOperator<O, S> {
       throw new Error('wat can\'t compose empty list')
     }
 
+    // do some quick checks
+    let index = first(operations).index
+    for (let op of operations) {
+      if (op.index !== index) {
+        throw new Error('wat, indices out of order')
+      }
+      index = op.nextIndex
+    }
+
     let composedOs: O[] = this.operator.composeMany(map(o => o.subops, operations))
     return {
       subops: composedOs,
       operationId: genUid(),
-      parentHash: first(operations).parentHash,
-      childHash: last(operations).childHash,
+      index: first(operations).index,
+      nextIndex: last(operations).nextIndex,
     }
   }
 
-  transform (
+  transformServerOp (
     a: ContextualOperation<O>, // client op
     b: ContextualOperation<O>, // server op
-    params: {
-      aT?: ContextualOperation<O>,
-      bT?: ContextualOperation<O>,
-    }
-  ): [ContextualOperation<O>, ContextualOperation<O>] { // returns [aT, bT]
+    aT: ContextualOperation<O>,
+  ): ContextualOperation<O> { // returns bT
     //   a /\ b
     //    /  \
     // bP \  / aP
     //     \/
 
-    if (a.parentHash !== b.parentHash) {
+    if (a.index !== b.index) {
       throw new Error('wat, parent hashes must match')
     }
 
-    let [aT, bT] = [params.aT, params.bT]
-
-    if (aT && bT) {
-      throw new Error('wat, why are you transforming')
-    }
-
-    let resultHash
-    if (aT) {
-      resultHash = aT.childHash
-    } else if (bT) {
-      resultHash = bT.childHash
-    } else {
-      throw new Error('wat, you need to pass in one half of the transform')
-    }
+    let resultIndex = aT.nextIndex
 
     let [_aT, _bP] = this.operator.transform(a.subops, b.subops)
 
-    return [
-      aT || {
-        subops: _aT,
-        operationId: a.operationId,
-        parentHash: b.childHash,
-        childHash: resultHash,
-      },
-      bT || {
-        subops: _bP,
-        operationId: b.operationId,
-        parentHash: a.childHash,
-        childHash: resultHash,
-      }
-    ]
-  }
-
-  transform2nd (
-    a: ContextualOperation<O>, // client op
-    b: ContextualOperation<O>, // server op
-    aT: ContextualOperation<O>
-  ): ContextualOperation<O> { // returns bP
-    let [_aT, _bT] = this.transform(a, b, { aT: aT })
-    return _bT
-  }
-
-  transform1st (
-    a: ContextualOperation<O>, // client op
-    b: ContextualOperation<O>, // server op
-    bT: ContextualOperation<O>
-  ): ContextualOperation<O> { // returns aP
-    let [_aT, _bT] = this.transform(a, b, { bT: bT })
-    return _aT
+    return {
+      subops: _bP,
+      operationId: b.operationId,
+      index: a.nextIndex,
+      nextIndex: resultIndex,
+    }
   }
 
   transformAndApply (
@@ -166,46 +125,43 @@ export class ContextualOperator<O, S> {
 
     let [a, b] = [clientOp, serverOp]
 
-    if (a.parentHash !== b.parentHash) {
+    if (a.index !== b.index) {
       throw new Error('wat, parent hashes must match')
     }
 
     let [aT, bT] = this.operator.transform(a.subops, b.subops)
 
-    let newState, newHash
+    let newState
     if (params.clientState != null) {
       newState = this.applier.apply(params.clientState, bT)
-      newHash = this.applier.stateHash(newState)
     } else if (params.serverState != null) {
       newState = this.applier.apply(params.serverState, aT)
-      newHash = this.applier.stateHash(newState)
     } else {
       throw new Error('need client or server state to apply transformed op to')
+    }
+
+    let newIndex
+    if (clientOp.subops.length > 0) {
+      newIndex = serverOp.nextIndex + 1
+    } else {
+      newIndex = serverOp.nextIndex
     }
 
     return [
       {
         subops: aT,
         operationId: a.operationId,
-        parentHash: b.childHash,
-        childHash: newHash,
+        index: b.nextIndex,
+        nextIndex: newIndex,
       },
       {
         subops: bT,
         operationId: b.operationId,
-        parentHash: a.childHash,
-        childHash: newHash,
+        index: a.nextIndex,
+        nextIndex: newIndex,
       },
       newState
     ]
-  }
-}
-
-export class ClientOrchestrator<O,S> {
-  client: Client<O,S>
-
-  constructor(client: Client<O,S>) {
-    this.client = client
   }
 }
 
@@ -216,7 +172,6 @@ export class Client<O,S> {
   uid: SiteUid
 
   state: S
-  stateHash: StateHash
 
   bufferedOperation: ContextualOperation<O>
   // the client ops not yet sent to the server.
@@ -229,7 +184,6 @@ export class Client<O,S> {
       uid: SiteUid,
 
       state: S,
-      stateHash: StateHash,
 
       bufferedOperation: ContextualOperation<O>,
       // the client ops not yet sent to the server.
@@ -243,32 +197,29 @@ export class Client<O,S> {
 
     if (data == null) {
       let state = applier.initial()
-      let hash = applier.stateHash(state)
       data = {
         uid: genUid(),
         state: state,
-        stateHash: applier.stateHash(state),
-        bufferedOperation: operator.empty(hash),
-        sentOperation: operator.empty(hash),
+        bufferedOperation: operator.empty(0),
+        sentOperation: operator.empty(0),
       }
     }
 
     Object.assign(this, data)
   }
 
+  nextIndex(): number {
+    return this.bufferedOperation.nextIndex
+  }
+
   _checkInvariants () {
     let clientState = this.state
-    let clientStateHash = this.stateHash
 
     let prebuffer = this.sentOperation
     let buffer = this.bufferedOperation
 
-    if (prebuffer.childHash !== buffer.parentHash) {
+    if (prebuffer.nextIndex !== buffer.index) {
       throw new Error('prebuffer should point to buffer')
-    }
-
-    if (buffer.childHash !== clientStateHash) {
-      throw new Error('buffer should point to current state')
     }
   }
 
@@ -276,26 +227,25 @@ export class Client<O,S> {
     let serverOp = serverBroadcast.operation
 
     let clientState = this.state
-    let clientStateHash = this.stateHash
 
     if (this.sentOperation.subops.length > 0) {
       return // we're waiting for a response elsewhere... ignore this update
     }
 
-    if (serverOp.parentHash !== clientStateHash) {
+    if (this.bufferedOperation.subops.length > 0) {
+      throw new Error('wat, why buffer if there\'s no sent?')
+    }
+
+    if (serverOp.index !== this.nextIndex()) {
       throw new Error('wat') // figure out what to do!
     }
 
-    let serverO = serverOp.subops
-
-    let newState = this.applier.apply(clientState, serverO)
-    let newStateHash = this.applier.stateHash(newState)
-
-    this.state = newState
-    this.stateHash = newStateHash
+    this.sentOperation = this.operator.empty(serverOp.nextIndex)
+    this.bufferedOperation = this.operator.empty(serverOp.nextIndex)
+    this.state = this.applier.apply(clientState, serverOp.subops)
   }
 
-  _flushBuffer(): ?ClientEditRequest<O> {
+  _flushBuffer(): ?ClientEdit<O> {
     // if there's no buffer, skip
     if (this.bufferedOperation.subops.length === 0) {
       return undefined
@@ -307,21 +257,21 @@ export class Client<O,S> {
     }
 
     // flush the buffer!
-    let editRequest: ClientEditRequest<O> = {
-      kind: 'ClientEditRequest',
+    let editRequest: ClientEdit<O> = {
+      kind: 'ClientEdit',
       operation: this.bufferedOperation
     }
 
     // prebuffer is now the buffer
     this.sentOperation = this.bufferedOperation
-    this.bufferedOperation = this.operator.empty(this.stateHash)
+    this.bufferedOperation = this.operator.empty(this.nextIndex())
 
     this._checkInvariants()
 
     return editRequest
   }
 
-  handleServerResponse(serverResponse: ServerEditResponse<O>): ?ClientEditRequest<O> {
+  handleServerResponse(serverResponse: ServerEditResopnse<O>): ?ClientEdit<O> {
     // client history is [a, c] where
     //   a: the op we've sent the server
     //   c: the op we've buffered and not yet sent to the server
@@ -348,10 +298,9 @@ export class Client<O,S> {
     let aP = serverResponse.transformedOperation
     let c = this.bufferedOperation
 
-    let serverHash = aP.childHash
+    let serverIndex = aP.nextIndex
 
     let clientState = this.state
-    let clientHash = this.stateHash
 
     if (a == null) {
       throw new Error('how are we getting an edit response w/o outgoing edit?')
@@ -362,27 +311,25 @@ export class Client<O,S> {
     }
 
     // transform & apply!
-    let bP = this.operator.transform2nd(a, b, aP)
+    let bP = this.operator.transformServerOp(a, b, aP)
     let [cP, bPP, newState] = this.operator.transformAndApply(
       c, bP, { clientState: clientState })
-    let newHash = this.applier.stateHash(newState)
 
-    if (newHash !== bPP.childHash || newHash !== cP.childHash) {
+    if (this.nextIndex() !== bPP.nextIndex || this.nextIndex() !== cP.nextIndex) {
       throw new Error('we got to the wrong end state...')
     }
 
     // update the new state
     this.state = newState
-    this.stateHash = newHash
 
     // ack that we've handled the sent buffer
-    this.sentOperation = this.operator.empty(aP.childHash)
+    this.sentOperation = this.operator.empty(cP.index)
     this.bufferedOperation = cP
 
     //
 
     this._checkInvariants()
-    if (this.bufferedOperation.parentHash !== serverHash) {
+    if (this.bufferedOperation.index !== serverIndex) {
       throw new Error('the buffer should now be parented on the server state')
     }
 
@@ -391,23 +338,19 @@ export class Client<O,S> {
   }
 
   localEdit(ops: O[])
-  : ?ClientEditRequest<O> { // return client op to broadcast
+  : ?ClientEdit<O> { // return client op to broadcast
     // apply the operation
     let clientState = this.state
-    let clientStateHash = this.stateHash
-
     let newState = this.applier.apply(clientState, ops)
-    let newStateHash = this.applier.stateHash(newState)
 
     this.state = newState
-    this.stateHash = newStateHash
 
     // the op we just applied!
     let op: ContextualOperation<O> = {
       subops: ops,
       operationId: genUid(),
-      parentHash: clientStateHash,
-      childHash: newStateHash
+      index: this.nextIndex(),
+      nextIndex: this.nextIndex() + 1
     }
 
     // append operation to buffer (& thus bridge)
@@ -428,6 +371,8 @@ export class Server<O,S> {
 
   state: S
   log: Array<ContextualOperation<O>> // history of operations, oldest to newest
+
+  clientAckIndex: {[clientUid: SiteUid]: number}
 
   constructor(operator: ContextualOperator<O,S>, applier: IApplier<O,S>,
     data?: {
@@ -451,33 +396,26 @@ export class Server<O,S> {
     Object.assign(this, data)
   }
 
-  _historyBetween(startHash: StateHash, endHash: StateHash): Array<ContextualOperation<O>> {
-    if (endHash === startHash) { return [] }
-
-    let i = findLastIndex(o => o.parentHash === startHash, this.log)
-    if (i == null) { throw new Error('wat') }
-
-    let ops = Array.from(subarray(this.log, {start: i})())
-    if (ops.length === 0) { throw new Error('wat') }
-
-    if (first(ops).parentHash !== startHash) { throw new Error('wat') }
-    if (last(ops).childHash !== endHash) { throw new Error('wat') }
-
-    return ops
+  nextIndex(): number {
+    return this.log.length
   }
 
-  _historyOp(startHash: StateHash): ContextualOperation<O> {
-    let endHash = this.applier.stateHash(this.state)
-    if (endHash === startHash) {
-      return this.operator.empty(endHash)
-    } else {
-      let historyOps = this._historyBetween(startHash, endHash)
-      return this.operator.composeFull(historyOps)
+  lastIndex(): number {
+    return this.log.length - 1
+  }
+
+  _historyOp(startIndex: number): ContextualOperation<O> {
+    if (startIndex === this.nextIndex()) {
+      return this.operator.empty(startIndex)
     }
+
+    let ops = Array.from(subarray(this.log, {start: startIndex})())
+    if (ops.length === 0) { throw new Error('wat') }
+    return this.operator.composeFull(ops)
   }
 
-  handleClientRequest(clientRequest: ClientEditRequest<O>)
-  : [ServerEditResponse<O>, ServerBroadcast<O>] {
+  handleClientEdit(clientRequest: ClientEdit<O>)
+  : [ServerEditResopnse<O>, ServerBroadcast<O>] {
     //   a /\ b
     //    /  \
     // bP \  / aP
@@ -485,7 +423,7 @@ export class Server<O,S> {
 
     let clientOp = clientRequest.operation
 
-    let historyOp: ContextualOperation<O> = this._historyOp(clientOp.parentHash)
+    let historyOp: ContextualOperation<O> = this._historyOp(clientOp.index)
 
     let [a, b] = [clientOp, historyOp]
     let [aP, bP, newState] = this.operator.transformAndApply(
@@ -496,7 +434,7 @@ export class Server<O,S> {
 
     return [
       {
-        kind: 'ServerEditResponse',
+        kind: 'ServerEditResopnse',
         intermediateOperation: b,
         transformedOperation: aP
       },
@@ -511,10 +449,10 @@ export class Server<O,S> {
 type Packet<O,S> = {
   sourceUid: SiteUid,
   destinationUid: SiteUid,
-  data: ServerEditResponse<*>
+  data: ServerEditResopnse<*>
       | ServerBroadcast<*>
       | ServerResetResponse<*>
-      | ClientEditRequest<*>
+      | ClientEdit<*>
 }
 
 export class NetworkSimulator<O,S> {
@@ -533,72 +471,12 @@ export class NetworkSimulator<O,S> {
     this.clients[client.uid] = client
   }
 
-  request(clientUid: SiteUid, clientRequest: ClientEditRequest<*>) {
+  request(clientUid: SiteUid, clientRequest: ClientEdit<*>) {
     this.packets.push({
       data: clientRequest,
       sourceUid: clientUid,
       destinationUid: this.server.uid
     })
-  }
-
-  * _handlePacket (packet: Packet<O,S>): Iterator<Packet<O,S>> {
-    let data = packet.data
-    let originatingUid = packet.sourceUid
-    let handlerUid = packet.destinationUid
-
-    switch (data.kind) {
-      case 'ClientEditRequest': {
-        // handle it on the server
-        let [response, broadcast] = this.server.handleClientRequest(data)
-
-        if (handlerUid !== this.server.uid) {
-          throw new Error('wat, client should only talk to server')
-        }
-
-        // broadcast update to other clients
-        for (let clientUid of Object.keys(this.clients)) {
-          if (clientUid === originatingUid) {
-            continue
-          }
-          yield {
-            sourceUid: handlerUid,
-            destinationUid: clientUid,
-            data: broadcast
-          }
-        }
-
-        // send response to originating client
-        yield {
-          sourceUid: handlerUid,
-          destinationUid: originatingUid,
-          data: response
-        }
-        break
-      }
-      case 'ServerBroadcast': {
-        // handle it on the client
-        let client = this.clients[handlerUid]
-        client.handleServerBroadcast(data)
-        break
-      }
-      case 'ServerEditResponse': {
-        // handle it on the client
-        let client = this.clients[handlerUid]
-        let request = client.handleServerResponse(data)
-        if (request == null) { break }
-
-        // send response to the server
-        yield {
-          sourceUid: handlerUid,
-          destinationUid: originatingUid,
-          data: request
-        }
-        break
-      }
-      default: {
-        throw new Error('unknown packet')
-      }
-    }
   }
 
   deliverPackets(num: number) {
